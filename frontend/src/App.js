343ebea95aec892233d5d9281ac32f8aa5b40bb0
ShearmanChua <shearman_chua@hotmail.com>
2025-06-12 10:42:51 +0800

    revert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import IncidentCards from './components/IncidentCards';
-import ChatAssistant from './components/ChatAssistant';
 import './App.css';
 
 function App() {
@@ -21,8 +20,6 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/incidents" element={<IncidentCards />} />
         </Routes>
-
-        <ChatAssistant />
       </div>
     </Router>
   );
